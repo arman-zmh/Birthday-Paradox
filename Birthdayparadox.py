32236815bd3201251bda14e22e5d2607002c6569
arman-zmh <arman.zamaniha123@gmail.com>
2026-06-07 17:08:31 +0330

    defining Get Birthday function

--- Birthdayparadox.py
+++ Birthdayparadox.py
@@ -1,1 +1,14 @@
 import datetime, random
+
+def getBirthdays(numberOfBirthdays):
+
+    birthdays = []
+
+    for i in range(numberOfBirthdays):
+
+        startOfTheYear = datetime.date(2001,1,1)
+
+        randomNumberOfDays = datetime.timedelta(random.randint(0, 364))
+        birthday = startOfTheYear + randomNumberOfDays
+        birthdays.append(birthday)
+    return birthdays
