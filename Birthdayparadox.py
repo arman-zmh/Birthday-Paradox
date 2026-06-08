d8c6761b49806e74cfcc0ad25ac6a10abafdc0b7
arman-zmh <arman.zamaniha123@gmail.com>
2026-06-08 19:48:09 +0330

    getMach function

--- Birthdayparadox.py
+++ Birthdayparadox.py
@@ -12,3 +12,12 @@ def getBirthdays(numberOfBirthdays):
         birthday = startOfTheYear + randomNumberOfDays
         birthdays.append(birthday)
     return birthdays
+def getMatch(birthdays):
+
+    if len(birthdays) ==len(set(birthdays)):
+        return None
+    for a, birthdayA in enumerate(birthdays):
+        for b, birthayB in enumerate(birthdays[a+1:]):
+            if birthdayA == birthayB:
+                return birthdayA
+            
