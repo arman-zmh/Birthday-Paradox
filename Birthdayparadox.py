9c0e0e7c9ef363f217c08b0d488fe31fca3ef65b
arman-zmh <arman.zamaniha123@gmail.com>
2026-06-09 16:35:32 +0330

    while is written

--- Birthdayparadox.py
+++ Birthdayparadox.py
@@ -21,3 +21,37 @@ def getMatch(birthdays):
             if birthdayA == birthayB:
                 return birthdayA
             
+print('''Birthday Paradox, by Arman-zmh
+
+The Birthday Paradox shows us that in a group of N people, the odds
+that two of them have matching birthdays is surprisingly large. ''')
+
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
+while True: 
+    print('How many birthdays shall I generate? (Max 100)')
+
+    respone = input('> ')
+    if respone.isdecimal() and ( 0 < int(respone) =< 100):
+        numBDays = int(respone)
+        break
+
+print('Here are', numBDays, 'birthdays:')
+
+birthdays = getBirthdays(numBDays)
+for i, birthday in enumerate(birthdays):
+    if i != 0:
+        print(', ', end='')
+
+    monthName = MONTHS(birthday.month - 1)
+    dateText = '{} {}'.format(monthName, birthday.day)
+
+    print(dateText, end='')
+
+print()
+
+match = getMatch(birthdays)
+
+print('In this simulation, ', end='')
+
